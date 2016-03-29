70ff486ff1a9a79e9c8e9de2e361f1465bf9a090
Mark Knol <mark.knol@gmail.com>
2016-03-29 22:56:16 +0200

    use lessc to minify/validate CSS

--- build-site.bat
+++ build-site.bat
@@ -1,4 +1,7 @@
 @echo off
+call lessc assets/includes/css/github.css assets/includes/css/github.min.css --clean-css="--s1 --advanced"
+call lessc assets/includes/css/haxe-nav.css assets/includes/css/haxe-nav.min.css --clean-css="--s1 --advanced"
+call lessc assets/includes/css/styles.css assets/includes/css/styles.min.css --clean-css="--s1 --advanced"
 rmdir "output" /S /Q
 mkdir "output"
 neko CodeCookBook.n
