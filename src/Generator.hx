25145215400e9ce6e2cbabbfbfdd36fe742402bc
Mark Knol <mark.knol@gmail.com>
2016-04-11 21:52:17 +0200

    Don't generate all-tags.txt

--- src/Generator.hx
+++ src/Generator.hx
@@ -83,8 +83,8 @@ class Generator {
       }
     });
     
-    var allTags = [for (tag in tags.keys()) tag];
-    File.saveContent("used-tags.txt", allTags.join("\r\n"));
+    //var allTags = [for (tag in tags.keys()) tag];
+    //File.saveContent("used-tags.txt", allTags.join("\r\n"));
     
     trace(sitemap.length + " categories");
     trace(allTags.length + " tags");
