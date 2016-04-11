3fea8fe31b0002429d53ff7a7ceaddd9f9438c16
Xavier Van de Woestyne <xavier.vdw@gmail.com>
2016-04-11 21:51:23 +0200

    Append a builder for Linux/OSX and gaft the footer on the bottom of the page (using flex) (#27)

---
+++ build-site.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+lessc assets/includes/css/github.css assets/includes/css/github.min.css --clean-css="--s1 --advanced"
+lessc assets/includes/css/haxe-nav.css assets/includes/css/haxe-nav.min.css --clean-css="--s1 --advanced"
+lessc assets/includes/css/styles.css assets/includes/css/styles.min.css --clean-css="--s1 --advanced"
+rm -rf output
+mkdir output
+neko CodeCookBook.n
