5efd31722f4d986bfe0aae032cc261f09f75380e
Mark Knol <mark.knol@gmail.com>
2016-08-26 16:06:27 +0200

    Update deploy.sh
    nicer deployment

--- deploy.sh
+++ deploy.sh
@@ -16,19 +16,19 @@ cd output
 git init
 
 # inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI"
-git config user.email "nadako@gmail.com"
+git config user.name "${GHP_BOT_NAME}"
+git config user.email "${GHP_BOT_EMAIL}"
 
 # add the CNAME file
-echo code.haxe.org > CNAME
+echo "${GHP_CNAME}" > CNAME
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -m "Deploy to GitHub Pages from ${TRAVIS_COMMIT}"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@github.com/HaxeFoundation/code-cookbook.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GHP_BOT_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1 && echo done
