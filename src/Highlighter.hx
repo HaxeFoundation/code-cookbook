2c04d1a14ba6b24d85b0fdaad6f6612d417ab9ca
Mark Knol <mark.knol@gmail.com>
2016-09-01 18:21:01 +0200

    update syntax highlighter
    * remove jquery dependency
    * fix `// comments` for some failing cases
    * add hxml highlighter

--- src/Highlighter.hx
+++ src/Highlighter.hx
@@ -1,18 +1,28 @@
 package;
+import js.Browser.document;
+import js.html.Element;
 
 /**
  * @author Mark Knol
  */
-class Highlighter{
+class Highlighter {
   public static function main() {
-    new js.JQuery("code.prettyprint").each(function() {
-      var el = js.JQuery.cur;
-      if (!el.hasClass("highlighted") && (el.hasClass("haxe") || el.hasClass("js") || el.hasClass("javascript"))) {
-        el.html(syntaxHighlight(el.html()));
-        el.addClass("highlighted");
+    for (el in document.getElementsByTagName("code")) {
+      if (hasClass(el, "prettyprint")) {
+        if (!hasClass(el, "highlighted")) {
+          if (hasClass(el, "haxe") || hasClass(el, "js") || hasClass(el, "javascript")) {
+            el.innerHTML = syntaxHighlight(el.innerHTML);
+            el.className += " highlighted";
+          } else if (hasClass(el, "hxml")) {
+            el.innerHTML = syntaxHighlightHXML(el.innerHTML);
+            el.className += " highlighted";
+          }
+        }
       }
-    });
+    }
   }
+
+  static function hasClass(el:Element, className:String) return el.className.indexOf(className) != -1;
 
   static function syntaxHighlight(html:String) {
     var kwds = ["abstract", "trace", "break", "case", "cast", "class", "continue", "default", "do", "dynamic", "else", "enum", "extends", "extern", "for", "function", "if", "implements", "import", "in", "inline", "interface", "macro", "new", "override", "package", "private", "public", "return", "static", "switch", "throw", "try", "typedef", "untyped", "using", "var", "while" ];
@@ -28,8 +38,17 @@ class Highlighter{
     html = types.replace(html, "<span class='type'>$1</span>");
     
     html = ~/("[^"]*")/g.replace(html, "<span class='str'>$1</span>");
-    html = ~/(\/\/.+\n)/g.replace(html, "<span class='cmt'>$1</span>");
+    html = ~/(\/\/.+?)(\n|$)/g.replace(html, "<span class='cmt'>$1</span>$2");
     html = ~/(\/\*\*?[^*]*\*?\*\/)/g.replace(html, "<span class='cmt'>$1</span>");
+    
+    return html;
+  }
+
+  static function syntaxHighlightHXML(html:String) {
+    html = ~/\b(haxe)\b/g.replace(html, "<span class='kwd'>$1</span>");
+    html = ~/("[^"]*")/g.replace(html, "<span class='str'>$1</span>");
+    html = ~/(--?.+?)(\s)/g.replace(html, "<span class='val'>$1</span>$2");
+    html = ~/(#.+?)(\n|$)/g.replace(html, "<span class='cmt'>$1</span>$2");
     
     return html;
   }
