a2549294248570f802650687cb4bc70a7d89b7f6
Mark Knol <mark.knol@gmail.com>
2016-09-09 10:16:50 +0200

    We don't need no batch files.
    * everything can be done in the hxml :)

--- build-site.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call lessc assets/includes/css/haxe-nav.css assets/includes/css/haxe-nav.min.css --clean-css="--s1 --advanced"
-call lessc assets/includes/css/styles.css assets/includes/css/styles.min.css --clean-css="--s1 --advanced"
-rmdir "output" /S /Q
-mkdir "output"
-neko CodeCookBook.n
--- build-site.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-lessc assets/includes/css/haxe-nav.css assets/includes/css/haxe-nav.min.css --clean-css="--s1 --advanced"
-lessc assets/includes/css/styles.css assets/includes/css/styles.min.css --clean-css="--s1 --advanced"
-rm -rf output
-mkdir output
-neko CodeCookBook.n
