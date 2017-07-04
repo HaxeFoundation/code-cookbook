c54d3d0784a5fbde4a2f781437668c4b8b39f55e
Mark Knol <mark.knol@gmail.com>
2017-07-04 13:42:01 +0200

    more Haxe programming language

--- src/Main.hx
+++ src/Main.hx
@@ -7,7 +7,7 @@ class Main {
   public static function main() {
     haxe.Timer.measure(function() {
       var generator = new Generator();
-      generator.titlePostFix = " - Haxe Code Cookbook";
+      generator.titlePostFix = " - Haxe programming language cookbook";
       generator.basePath = "http://code.haxe.org/";
       generator.repositoryUrl = "https://github.com/HaxeFoundation/code-cookbook/";
       generator.repositoryBranch = "master";
